3965e26f66ed0ff36d44bdbc2db96b862982e43f
Josh Graff <github@joshuagraff.com>
2023-02-25 14:15:46 -0500

    add other definitions

--- read_bcd.py
+++ read_bcd.py
@@ -1,5 +1,11 @@
 import bcd
 
 
-temp_zero = bcd.BCD(7, 23, 24, 25, 8)
-print("current value: ", temp_zero.value())
+weight_ones = bcd.BCD(7, 23, 24, 25, 8)
+weight_tens = bcd.BCD(1, 23, 24, 25, 8)
+
+
+temp_ones = bcd.BCD(12, 23, 24, 25, 8)
+temp_tens = bcd.BCD(16, 23, 24, 25, 8)
+temp_hundreds = bcd.BCD(20, 23, 24, 25, 8)
+print("current value: ", weight_tens.value(), weight_ones.value())
