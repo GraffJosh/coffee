2373adc9a2e01f8081443aeec46385baaa916d91
Josh Graff <github@joshuagraff.com>
2023-02-25 16:01:24 -0500

    update bcd pins

--- read_bcd.py
+++ read_bcd.py
@@ -6,7 +6,7 @@ weight_tens = bcd.BCD(25, 8, 7, 1)
 
 
 temp_ones = bcd.BCD(12, 16, 20, 21)
-temp_tens = bcd.BCD(14, 15, 23, 24)
+temp_tens = bcd.BCD(14, 15, 24, 23)
 temp_hundreds = bcd.BCD(26, 19, 13, 6)
 print("Weight Selection: ", weight_tens.value(), weight_ones.value())
 print("Temp Selection: ", temp_hundreds.value(), temp_tens.value(), temp_ones.value())
